d8fe4e6cb414412b2a203df29252fe59fb028986
Yama022 <r-jacquemin@hotmail.fr>
2022-07-25 14:40:09 +0200

    Add login.sql + connection function on login.js node

---
+++ login/src/components/nodelogin/login.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodelogin'
+});
