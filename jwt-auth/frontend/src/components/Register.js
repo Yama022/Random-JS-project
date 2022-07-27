e7a272a32189a82e678ea4cc8e0e7cdc4e1358b1
Yama022 <r-jacquemin@hotmail.fr>
2022-07-27 10:44:20 +0200

    Add content on Login.js + update Register.js

--- jwt-auth/frontend/src/components/Register.js
+++ jwt-auth/frontend/src/components/Register.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
-import './style.scss';
 
 export default function Register() {
   const [name, setName] = useState('');
